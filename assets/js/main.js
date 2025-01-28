c02e87ad6dfe7c12d53788442882d3614f41eda6
Anton Nikolaichuk <109733943+per5ect@users.noreply.github.com>
2025-01-28 21:33:41 +0100

    create portfolio
    create portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const burgerFunc = () => {
+  const headerToggleBtn = document.querySelector('.header-toggle');
+
+  function headerToggle() {
+    document.querySelector('#header').classList.toggle('header-show');
+    headerToggleBtn.classList.toggle('bi-list');
+    headerToggleBtn.classList.toggle('bi-x');
+  }
+  headerToggleBtn.addEventListener('click', headerToggle);
+
+};
